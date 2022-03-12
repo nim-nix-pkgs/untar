45eb6de61ecfb6b7da9873d9c63c5555161a10d6
hugosenari <hugosenari@gmail.com>
2022-03-12 01:46:09 -0300

    chore: re index untar

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Library for decompressing tar.gz files.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-untar-master.flake = false;
+  inputs.src-untar-master.owner = "dom96";
+  inputs.src-untar-master.ref   = "refs/heads/master";
+  inputs.src-untar-master.repo  = "untar";
+  inputs.src-untar-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-untar-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-untar-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
